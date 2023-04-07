c07d8481a4b843143cc1e6f83a8580693f8f55ff
Andreja Nesic <office@andrejanesic.com>
2023-04-07 21:33:44 +0200

    Multiple config running edit

--- genetic_algorithm_neural_network_training/common/config.py
+++ genetic_algorithm_neural_network_training/common/config.py
@@ -1,36 +1,37 @@
 # -*- coding: utf-8 -*-
 
 from .args import args
+from typing import Optional
 import random
 import configparser
 
 __config = None
 
-def config() -> configparser.ConfigParser:
+def config(config_src: Optional[str]) -> configparser.ConfigParser:
     """
     Returns the program configuration, or loads the configuration if not loaded
     yet and returns.
     """
     global __config
 
-    if __config != None:
+    if config_src == None and __config != None:
         return __config
     
     __config = configparser.ConfigParser()
-    __config.read(args().get_config_src())
+    __config.read(config_src)
 
     # TODO document this
     __config["DEFAULT"] = {
         "pop_size": 30,
-        "sol_lb": -6,
-        "sol_ub": 6,
-        "sel_perc": 40,
+        "sol_lb": -5,
+        "sol_ub": 5,
+        "sel_perc": 50,
         "mut_perc": 10,
         "mut_n": 1,
         "indiv_l": 33,
         "max_iter": 150,
         "out_file": "sys.stdout",
-        "runs": 1,
+        "runs": 3,
         "random_seed": 123_123_123,
     }
 
